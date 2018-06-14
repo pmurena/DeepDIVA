14f493594ef88e23243d80d01a95c1eb0c89f9a9
Patrick Murena <patrick.murena@gmail.com>
2018-06-14 14:35:46 +0200

    changed to one file corpus

--- util/data/get_a_dataset.py
+++ util/data/get_a_dataset.py
@@ -269,7 +269,7 @@ def wiki(args):
     wiki_path = os.path.join(args.output_folder, 'wiki')
     _make_folder_if_not_exists(wiki_path)
 
-    # Download archive
+    '''    # Download archive
     source = 'https://dumps.wikimedia.org/enwiki/latest/'
     source += 'enwiki-latest-pages-articles.xml.bz2'
     responce = urllib.request.urlretrieve(
@@ -295,7 +295,7 @@ def wiki(args):
     sys.argv.extend(['-o', wiki_path])
     WikiExtractor.main()
     sys.argv = list(tmp_argv)
-
+    '''
     # Make clean copies of files, no empty, lines, no tags.
     files = [
         os.path.join(ls[0], f)
@@ -303,14 +303,15 @@ def wiki(args):
         for f in ls[2]
         if re.match('wiki_[0-9]{2}', f)
     ]
-    for idx, file in enumerate(files):
-        sys.stdout.write('clean file {:.0%} done\r'.format((idx+1)/len(files)))
-        with open(file, 'r') as input_f:
-            f = input_f.read()
-            f = re.sub(r'<.*?>', '', f)
-            f = re.sub(r'\n{2,}', '\n', f)
-            with open('{}_{}'.format(file, 'clean'), 'w') as output_f:
-                output_f.write(f)
+    progress_msg = 'clean file {:.0%} done\r'
+    with open(os.path.join(wiki_path, 'wiki_dump_clean'), 'w') as output_f:
+        for idx, file in enumerate(files):
+            sys.stdout.write(progress_msg.format((idx+1)/len(files)))
+            with open(file, 'r') as input_f:
+                f = input_f.read()
+                f = re.sub(r'<.*?>', '', f)
+                f = re.sub(r'\n{2,}', '\n', f)
+            output_f.write(f)
 
     print('\nAll done, the corpus can be found in {}'.format(wiki_path))
     return
